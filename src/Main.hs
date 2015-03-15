849d3edae1a548b169acfbff85af053a308c5ea6
Heikki Aitakangas <ornedan@gmail.com>
2015-03-15 22:22:13 +0200

    Startup with logging

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
 import Configuration.Utils
+import Control.Lens
 import Control.Logging
+import Control.Concurrent
+import Control.Monad
+import Text.Printf
+import Data.Text
 
 import Config
 
@@ -11,8 +18,14 @@ mainInfo = programInfo "Dominions 4 game tracker IRC bot" pConfig defaultConfig
 
 main = runWithConfiguration mainInfo $ \conf -> do
   -- Set up logging
-  undefined
+  -- TODO: Modify `logging` to support multiple sinks (with separate log levels)
+  --       and log rotation (from fast-logger).
+  setLogTimeFormat "%Y-%m-%d %H:%M:%S"
+  let withLogging = case conf ^. logFile of
+                     ""        -> withStderrLogging
+                     otherwise -> withFileLogging (conf ^. logFile)
+  
+  withLogging $ do
 
-  -- Start components
-  undefined
-
+    forM_ [1 .. 20 :: Int] $ \n -> do
+      forkOS $ debugS "core" $ pack $ printf "derp %d" n
